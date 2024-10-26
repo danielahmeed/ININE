c4da7a622ca7ff40d28a92ab69cb0e73ce64d9f6
Daniel Ahmed <115207450+danielahmeed@users.noreply.github.com>
2024-10-26 20:31:25 +0530

    Add files via upload

---
+++ MOD2.h
@@ -0,0 +1,6 @@
+#pragma once 
+#include "k2c_tensor_include.h"
+#include "k2c_include.h"
+void MOD2(k2c_tensor* input_1_input, k2c_tensor* dense_output); 
+void MOD2_initialize();
+void MOD2_terminate();
